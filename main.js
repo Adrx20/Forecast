13f0aabac3c6a5cc2269124244bc4c24e91f2413
Adrx20 <vitaiandrii3@gmail.com>
2024-06-27 23:41:43 +0200

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ form1.addEventListener("submit", (event) => {
 	event.preventDefault();
 	let inp = document.getElementById("input1");
 	if (inp.value) {
-		window.location.href = 'index.html?city=' + inp.value;
+		window.location.href = 'main.html?city=' + inp.value;
 	}
 });
